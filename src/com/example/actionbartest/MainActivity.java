c8c41dd8aca8ba4e5dbb5275213a613e35cb8070
henry1 <1314hwl@163.com>
2016-05-26 17:42:37 +0800

    1.提交代码

---
+++ src/com/example/actionbartest/MainActivity.java
@@ -0,0 +1,59 @@
+package com.example.actionbartest;
+
+import com.example.actionbartest.view.ShadowActivity;
+
+import android.annotation.SuppressLint;
+import android.app.ActionBar;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.SearchView;
+
+public class MainActivity extends Activity {
+	private ActionBar mActionBar;
+	private ActionBar.Tab mDayTab;
+	private ActionBar.Tab mWeekTab;
+	private ActionBar.Tab mMonthTab;
+	private ActionBar.Tab mAgendaTab;
+	private SearchView mSearchView;
+	private MenuItem mSearchMenu;
+
+	@SuppressLint("NewApi")
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		mActionBar = this.getActionBar();
+		mActionBar.setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM | ActionBar.DISPLAY_SHOW_HOME
+				| ActionBar.DISPLAY_HOME_AS_UP | ActionBar.DISPLAY_USE_LOGO | mActionBar.DISPLAY_SHOW_TITLE);
+		mActionBar.setHomeButtonEnabled(false);
+		mActionBar.setDisplayHomeAsUpEnabled(false);
+		mActionBar.setDisplayShowHomeEnabled(false);
+		getActionBar().setIcon(android.R.color.transparent);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// TODO Auto-generated method stub
+		getMenuInflater().inflate(R.menu.action_menu, menu);
+		MenuItem mi = menu.findItem(R.id.action_search);
+		mSearchView = (SearchView) mi.getActionView();
+		return super.onCreateOptionsMenu(menu);
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		System.out.println("itemId:" + item.getItemId());
+		Intent it = new Intent();
+		switch (item.getItemId()) {
+		case R.id.view_shadow:
+			it.setClass(MainActivity.this, ShadowActivity.class);
+			startActivity(it);
+			break;
+		}
+
+		return super.onOptionsItemSelected(item);
+	}
+}
---
+++ src/com/example/actionbartest/view/ShadowActivity.java
@@ -0,0 +1,27 @@
+package com.example.actionbartest.view;
+
+import com.example.actionbartest.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * <title>shadow 的使用</title>
+ * <p>
+ * android:shadowColor="#ff000000" 设置阴影的颜色 <br>
+ * android:shadowDx="5" 设置x上的偏移,阴影取相反方向,x>0阴影在左边,x<0阴影在右边 <br>
+ * android:shadowDy="0" 设置y上的偏移,阴影取相反方向,y>0阴影在上边,y<0阴影在下边 <br>
+ * android:shadowRadius="1" Radius辐射光线<br>
+ * </p>
+ * 
+ * @author henry
+ *
+ */
+public class ShadowActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_shadow);
+	}
+}
